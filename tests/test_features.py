21c52996f309db94c8314eb3c6be263e1dd1ba31
Dallan Quass <dallan@quass.org>
2025-03-24 14:25:11 -0600

    make transactions in tests have same name

--- tests/test_features.py
+++ tests/test_features.py
@@ -3,7 +3,6 @@ import pytest
 
 from recur_scan.features import (
     get_ends_in_99,
-    get_is_always_recurring,
     get_is_insurance,
     get_is_phone,
     get_is_utility,
@@ -18,10 +17,10 @@ from recur_scan.transactions import Transaction
 def test_get_n_transactions_same_amount() -> None:
     """Test that get_n_transactions_same_amount returns the correct number of transactions with the same amount."""
     transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
+        Transaction(id=1, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=2, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=3, user_id="user1", name="name1", amount=200, date="2024-01-02"),
+        Transaction(id=4, user_id="user1", name="name1", amount=2.99, date="2024-01-03"),
     ]
     assert get_n_transactions_same_amount(transactions[0], transactions) == 2
     assert get_n_transactions_same_amount(transactions[2], transactions) == 1
@@ -33,10 +32,10 @@ def test_get_percent_transactions_same_amount() -> None:
     Tests that the function calculates the right percentage of transactions with matching amounts.
     """
     transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
+        Transaction(id=1, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=2, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=3, user_id="user1", name="name1", amount=200, date="2024-01-02"),
+        Transaction(id=4, user_id="user1", name="name1", amount=2.99, date="2024-01-03"),
     ]
     assert pytest.approx(get_percent_transactions_same_amount(transactions[0], transactions)) == 2 / 4
 
@@ -44,10 +43,10 @@ def test_get_percent_transactions_same_amount() -> None:
 def test_get_ends_in_99() -> None:
     """Test that get_ends_in_99 returns True for amounts ending in 99."""
     transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
+        Transaction(id=1, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=2, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=3, user_id="user1", name="name1", amount=200, date="2024-01-02"),
+        Transaction(id=4, user_id="user1", name="name1", amount=2.99, date="2024-01-03"),
     ]
     assert not get_ends_in_99(transactions[0])
     assert get_ends_in_99(transactions[3])
@@ -56,10 +55,10 @@ def test_get_ends_in_99() -> None:
 def test_get_n_transactions_same_day() -> None:
     """Test that get_n_transactions_same_day returns the correct number of transactions on the same day."""
     transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
+        Transaction(id=1, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=2, user_id="user1", name="name1", amount=100, date="2024-01-01"),
+        Transaction(id=3, user_id="user1", name="name1", amount=200, date="2024-01-02"),
+        Transaction(id=4, user_id="user1", name="name1", amount=2.99, date="2024-01-03"),
     ]
     assert get_n_transactions_same_day(transactions[0], transactions, 0) == 2
     assert get_n_transactions_same_day(transactions[0], transactions, 1) == 3
@@ -83,46 +82,25 @@ def test_get_n_transactions_days_apart() -> None:
 
 def test_get_is_insurance() -> None:
     """Test get_is_insurance."""
-    transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
-    ]
-    assert get_is_insurance(transactions[0])
-    assert not get_is_insurance(transactions[1])
+    assert get_is_insurance(
+        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01")
+    )
+    assert not get_is_insurance(Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"))
 
 
 def test_get_is_phone() -> None:
     """Test get_is_phone."""
-    transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
-    ]
-    assert get_is_phone(transactions[1])
-    assert not get_is_phone(transactions[2])
+    assert get_is_phone(Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"))
+    assert not get_is_phone(Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"))
 
 
 def test_get_is_utility() -> None:
     """Test get_is_utility."""
-    transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
-    ]
-    assert get_is_utility(transactions[2])
-    assert not get_is_utility(transactions[3])
+    assert get_is_utility(Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"))
+    assert not get_is_utility(
+        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03")
+    )
 
 
 def test_get_is_always_recurring() -> None:
     """Test get_is_always_recurring."""
-    transactions = [
-        Transaction(id=1, user_id="user1", name="Allstate Insurance", amount=100, date="2024-01-01"),
-        Transaction(id=2, user_id="user1", name="AT&T", amount=100, date="2024-01-01"),
-        Transaction(id=3, user_id="user1", name="Duke Energy", amount=200, date="2024-01-02"),
-        Transaction(id=4, user_id="user1", name="HighEnergy Soft Drinks", amount=2.99, date="2024-01-03"),
-    ]
-    assert not get_is_always_recurring(transactions[0])
